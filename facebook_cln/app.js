f80ad606083d8d44188fffcf894a1e985f287b00
x6galixia <x6galixia@gmail.com>
2024-08-18 14:15:02 +0800

    done eith database

--- facebook_cln/app.js
+++ facebook_cln/app.js
@@ -1,17 +1,50 @@
-const express = require("express")
-const app = express()
-const path = require("path")
+const express = require("express");
+const path = require("path");
+const Pool = require("pg").Pool;
 
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
-app.use(express.static(path.join(__dirname, "public")))
+const app = express();
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "facebook",
+    password: "1234",
+    port: "5432",
+});
+
+pool
+  .connect()
+  .then(() => console.log("Connected to facebook database"))
+  .catch((err) => console.error("Error connecting to facebook database:", err));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+// Middleware to parse URL-encoded bodies (for form data)
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/facebook/login", (req, res) => {
-	res.render("index")
-})
+    res.render("index");
+});
 
+app.post("/facebook/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        await pool.query(
+            "INSERT INTO users (username, password) VALUES ($1, $2)",
+            [username, password]
+        );
+        res.status(201).send("User added successfully");
+    } catch (error) {
+        console.error("Error inserting user:", error);
+        res.status(500).send("Error inserting user");
+    }
+});
 
 app.listen(8080, () => {
-    console.log("server listening on port 8080")
+    console.log("Server listening on port 8080");
 });
 
